63211299ea799f9b4be4c175d93d75f4b81f90f1
enghausen <morten@enghausen.dk>
2020-11-12 14:17:17 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -87,6 +87,7 @@ fi
 
 if [ ! -f /ebot/ssl/$DOMAIN/$DOMAIN.key ]
 then
+    mkdir -p /ebot/ssl/$DOMAIN
     /root/.acme.sh/acme.sh --issue --standalone -d $DOMAIN
     /root/.acme.sh/acme.sh --install-cert -d $DOMAIN --cert-file /ebot/ssl/$DOMAIN/$DOMAIN.cer --key-file /ebot/ssl/$DOMAIN/$DOMAIN.key --ca-file /ebot/ssl/$DOMAIN/ca.cer --fullchain-file /ebot/ssl/$DOMAIN/fullchain.cer --reloadcmd "service apache2 force-reload"
 fi
