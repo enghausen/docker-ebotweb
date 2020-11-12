dd555d1cc0e2d9a92047755d843cfd21a3d7aa95
enghausen <morten@enghausen.dk>
2020-11-12 11:24:24 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ MYSQL_HOST="${MYSQL_HOST:-mysql}"
 MYSQL_PORT="${MYSQL_PORT:-3306}"
 MYSQL_USER="${MYSQL_USER:-ebotv3}"
 MYSQL_PASS="${MYSQL_PASS:-ebotv3}"
-MYSQL_DB="${MYSQL_DB:-ebotv3}"
+MYSQL_DATABASE="${MYSQL_DATABASE:-ebotv3}"
 
 # eBotWeb settings (settings in app_user.yml)
 EBOT_WEB_HOME='/var/www/html'
@@ -48,7 +48,7 @@ while ! nc -z $MYSQL_HOST $MYSQL_PORT; do sleep 3; done
 
 if [ ! -f .installed ]
 then
-    php symfony configure:database "mysql:host=${MYSQL_HOST};dbname=${MYSQL_DB}" $MYSQL_USER $MYSQL_PASS
+    php symfony configure:database "mysql:host=${MYSQL_HOST};dbname=${MYSQL_DATABASE}" $MYSQL_USER $MYSQL_PASS
     php symfony doctrine:insert-sql
     php symfony guard:create-user --is-super-admin $EBOT_ADMIN_MAIL $EBOT_ADMIN_USER $EBOT_ADMIN_PASS
 
